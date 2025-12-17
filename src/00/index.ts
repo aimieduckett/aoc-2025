28a2c071a15bae5bdb118447cfefeb5a9088ecce
aimieduckett <aimieduckett@gmail.com>
2025-12-17 11:36:16 +0000

    Day 1 - part two

--- src/00/index.ts
+++ src/00/index.ts
@@ -1,14 +1,20 @@
 import fs from "node:fs";
+import path from "node:path";
 
 const DIAL_SIZE = 100; // 0 - 99
 let lines: string[] = [];
 
 try {
-  const data = fs.readFileSync("input.txt", "utf8");
+  const data = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
   lines = data.split("\n");
 } catch {
   console.error("error");
 }
+
+/**
+ * Part 1
+ * Number of times the dial points at 0 after a rotation
+ */
 
 let dialPosition = 50;
 let zeroCount = 0;
@@ -32,3 +38,38 @@ for (const line of lines) {
 }
 
 console.log("Zero count:", zeroCount);
+
+/**
+ * Part 2
+ * Number of times the dial points at 0 after any click
+ */
+
+let dialPosition2 = 50;
+let zeroCount2 = 0;
+
+for (const line of lines) {
+  const dir = line[0];
+  const amount = Number(line.slice(1));
+  const step = dir === "L" ? -1 : 1;
+
+  // avoid modulo in nested loop
+  for (let i = 0; i < amount; i++) {
+    // move one fixed click
+    dialPosition2 += step;
+
+    // wrap the circluar dial
+    if (dialPosition2 === DIAL_SIZE) dialPosition2 = 0; // 99 -> 100 -> 0
+    else if (dialPosition2 === -1) dialPosition2 = DIAL_SIZE - 1; // 0 -> -1 -> 99
+
+    if (dialPosition2 === 0) zeroCount2++;
+  }
+}
+
+console.log("Zero count:", zeroCount2);
+
+const totalIterations = lines.reduce(
+  (sum, line) => sum + Number(line.slice(1)),
+  0
+);
+
+console.log(totalIterations);
